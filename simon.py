2e171162c83d71c377f56cb58f341394f231c986
kevok <k.focke@tu-bs.de>
2019-12-22 01:30:38 +0100

    wir haben was gebastelt

--- simon.py
+++ simon.py
@@ -1,3 +1,22 @@
 import numpy as np
-bla = random.rand()
-print(bla)
+#bla = np.random.rand()
+#print(bla)
+a=[]
+b=[]
+c=[]
+t=100000000
+for i in range(t):
+	x=np.random.rand()
+	y=np.random.rand()
+	a.append(x)
+	b.append(y)
+
+
+for i in range(t):	
+	if a[i]**2+b[i]**2<1:
+		c.append(1)
+	
+u=4*float(len(c))/len(a)
+print(u)
+
+
